00fdb3b18590fe8cb6e09bc0b6468f3e7efb0cf6
HAYEONryu <21700470@handong.edu>
2021-09-20 15:24:23 +0900

    navigation fix

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,17 +1,16 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./Navigation.css";
 
-function Navigation(){
-    return (
-        <div>
-            <Link to="/">Home</Link>
-            <Link to={{
-                pathname: "/about",
-                state: {fromNavigation: true}
-            }}>About</Link>
-            {/* link는 라우터 안에만 있을 수 있다.  */}
-        </div>
-    );
+function Navigation() {
+  return (
+    <div className="nav">
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+     {/* link는 라우터 안에만 있을 수 있다.  */}
+
+    </div>
+  );
 }
+
 export default Navigation;
